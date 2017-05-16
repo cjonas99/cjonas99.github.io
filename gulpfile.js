e7ccc6e0129351d76a530682d4b4dfb765e8347d
Christiane <christiane@Christianes-MacBook-Pro.local>
2017-05-17 00:31:53 +0800

    Completed Pages
    added minify css to gulp; finished work pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 
 var sassFiles = 'assets/sass/**/*.scss',  
     cssDest = 'assets/css/';
@@ -9,6 +10,7 @@ gulp.task('styles', function(){
     gulp.src(sassFiles)
         .pipe(sass().on('error', sass.logError))
     	.pipe(concat('app.css'))
+    	.pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(cssDest));
 });
 
